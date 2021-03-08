f54c97914d01dc78e2cc535924bcec720fd97c3f
Erdal Karaca <erdal.karaca.de@gmail.com>
2021-03-08 20:57:36 +0100

    support REST path params in ajax calls

--- plugins/de.kcct.hi5/resources/hi5-io.js
+++ plugins/de.kcct.hi5/resources/hi5-io.js
@@ -7,8 +7,16 @@ define(['jquery'], function($) {
 		if (opts.data && typeof opts.data == 'object') {
 			opts.data = JSON.stringify(o.data);
 		}
-		if (opts.params) {
-			opts.url = opts.url + '?' + $.param(opts.params);
+		// check whether REST path params are provided and replace inline in URL
+		if (opts.pparams) {
+			Object.keys(opts.pparams).forEach(key => {
+				opts.url = opts.url.split('{' + key + '}').join(opts.pparams[key]);
+			});
+		}
+		// opts.params deprecated, caller should use opts.qparams
+		if (opts.params || opts.qparams) {
+			const params = {...opts.params, ...opts.qparams};
+			opts.url = opts.url + '?' + $.param(params);
 		}
 		return $.ajax(opts);
 	};
